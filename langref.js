30d78b726eb3889d3afc24dcfce68163db6ca4b1
Alexander Shoup <alexshoup@gmail.com>
2023-09-26 15:06:42 -0600

    Initial commit

---
+++ langref.js
@@ -0,0 +1,20 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.LangRef = void 0;
+exports.LangRef = [
+    { language: 'English', code: 'en' },
+    { language: 'Spanish', code: 'es' },
+    { language: 'French', code: 'fr' },
+    { language: 'German', code: 'de' },
+    { language: 'Chinese', code: 'zh' },
+    { language: 'Japanese', code: 'ja' },
+    { language: 'Italian', code: 'it' },
+    { language: 'Portuguese', code: 'pt' },
+    { language: 'Russian', code: 'ru' },
+    { language: 'Arabic', code: 'ar' },
+    { language: 'Hindi', code: 'hi' },
+    { language: 'Korean', code: 'ko' },
+    { language: 'Indonesian', code: 'id' },
+    { language: 'Latin', code: 'la' },
+    { language: 'Esperanto', code: 'eo' }
+];
---
+++ langref.ts
@@ -0,0 +1,22 @@
+interface LangRefType {
+  readonly language: string,
+  readonly code: string
+}
+
+export const LangRef: LangRefType[] = [
+  { language: 'English', code: 'en' },
+  { language: 'Spanish', code: 'es' },
+  { language: 'French', code: 'fr' },
+  { language: 'German', code: 'de' },
+  { language: 'Chinese', code: 'zh' },
+  { language: 'Japanese', code: 'ja' },
+  { language: 'Italian', code: 'it' },
+  { language: 'Portuguese', code: 'pt' },
+  { language: 'Russian', code: 'ru' },
+  { language: 'Arabic', code: 'ar' },
+  { language: 'Hindi', code: 'hi' },
+  { language: 'Korean', code: 'ko' },
+  { language: 'Indonesian', code: 'id' },
+  { language: 'Latin', code: 'la' },
+  { language: 'Esperanto', code: 'eo' }
+]
